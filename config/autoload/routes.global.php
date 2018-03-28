9b968431449b586dfc62defc044ff168d70f30ec
Daniel Dauber <daniel@portalqualis.com.br>
2018-03-28 20:55:03 -0300

    first commit

---
+++ config/autoload/routes.global.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+    'dependencies' => [
+        'invokables' => [
+            Zend\Expressive\Router\RouterInterface::class => Zend\Expressive\Router\AuraRouter::class,
+            App\Action\PingAction::class => App\Action\PingAction::class,
+
+        ],
+        'factories' => [
+            App\Action\HomePageAction::class => App\Action\HomePageFactory::class,
+            App\Action\Teste::class => App\Action\TesteFactory::class,
+        ],
+    ],
+
+    'routes' => [
+        [
+            'name' => 'home',
+            'path' => '/',
+            'middleware' => App\Action\HomePageAction::class,
+            'allowed_methods' => ['GET'],
+        ],
+        [
+            'name' => 'api.ping',
+            'path' => '/api/ping',
+            'middleware' => App\Action\PingAction::class,
+            'allowed_methods' => ['GET'],
+        ],
+        [
+            'name' => 'teste',
+            'path' => '/teste',
+            'middleware' => App\Action\Teste::class,
+            'allowed_methods' => ['GET'],
+        ],
+    ],
+];
